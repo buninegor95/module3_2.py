7ee80010622ec6331990955d3388a2bf19caa779
buninegor95 <buninegor95@gmail.com>
2024-10-25 22:16:47 +0300

    Add files via upload

---
+++ module3_2.py
@@ -0,0 +1,26 @@
+def  address_verification(adres):
+    if adres.find('@') == -1:
+        return False
+    if not (adres.find('.com', len(adres) - 5) != -1 or adres.find('.ru', len(adres) - 4) != -1 or adres.find('.net', len(adres) - 5) != -1):
+        return False
+    return  True
+
+
+
+def send_email(message, recipient, sender = 'university.help@gmail.com'):
+    if address_verification(recipient) == False or address_verification(sender) == False:
+        print("Невозможно отправить письмо с адреса ",sender," на адрес ",recipient,".")
+    elif  recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif  sender == 'university.help@gmail.com':
+        print("Письмо успешно отправлено с адреса ",sender," на адрес",recipient,".")
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо успешно отправлено с адреса ",sender," на адрес",recipient,".")
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
